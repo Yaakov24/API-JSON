2a030739214b419cbc5680593f0258769302ae08
YaakovG <ygoldenbergyo@gmail.com>
2024-05-02 09:47:50 -0400

    no json

---
+++ APIsAndJSON/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using RonVSKanyeAPI;
+namespace APIsAndJSON
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for(var i = 0; i < 5; i++)
+            {
+                //QuoteGenarater.KanyeQuote();
+                QuoteGenarater.RonQuote();
+            }
+           
+                OpenWeatherMapAPI.WeatherMap();
+            
+               
+            
+        }
+    }
+}
